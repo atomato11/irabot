1ceca0a309c8fd5c91fa80a38a09e7880d9ff2bf
limetreemain <tajtscp20@pm.me>
2022-01-02 13:43:37 +0000

    new file:   discord-bot/.gitignore 	modified:   discord-bot/deploy-commands.js

--- discord-bot/deploy-commands.js
+++ discord-bot/deploy-commands.js
@@ -6,11 +6,29 @@ const { clientId, guildID, token } = require('./config.json');
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
+const clientId = 'insertclientId';
+const guildId = 'insertguildId';
+
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
     commands.push(command.data.toJSON());
 }
 
+const rest = new REST({ version: '9' }).setToken(token);
+(async () => {
+    try {
+        console.log('(/)commands refresh')
+
+        await rest.put(
+            Routes.applicationGuildCommands(clientId, guildId),
+            { body: commands },
+        );
+
+        console.log('(/)commands refreshed');
+    } catch (error) {
+        console.error(error);
+    }
+})
 client.on('interactionCreate', async interaction => {
     if (!interaction.isCommand()) return;
 
