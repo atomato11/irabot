cf878fb62b30c97b0a678732a18f70560e7c4250
limetreemain <tajtscp20@pm.me>
2022-01-03 09:13:04 +0000

    modified:   discord-bot/commands/run.js

--- discord-bot/commands/run.js
+++ discord-bot/commands/run.js
@@ -1,17 +1,12 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { Client } = require('discord.js');
 
 const data = new SlashCommandBuilder()
     .setName('run')
     .setDescritpion('Run it to find out!')
-    .addSubcommand(subcommmand =>
-        subcommmand
-            .setName('Catch')
-            .setDescription('Suck')
-            .addUserOption(option => option.setName('target').setDescription('no')));
-        client.on('interactionCreate', interaction => {
-            if (!interaction.isCommand()) return;
-            
-            if (interaction.commandName === 'run') {
-                await interaction.reply('i am in your walls')
-            }
-        })
+Client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+    if (interaction.commandName ==='run') {
+        await interaction.reply({ content: 'i am in your walls', ephemeral: true });
+    }
+});
