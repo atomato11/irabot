901687eaddab9a8df78176b8e2ad456698f8cc5a
limetreemain <tajtscp20@pm.me>
2022-01-04 12:15:49 +0000

    modified:   discord-bot/commands/spam.js

--- discord-bot/commands/spam.js
+++ discord-bot/commands/spam.js
@@ -1,6 +1,113 @@
 const { SlashCommandBuilder } = requires('@discordjs/builder');
 const { Client } = requires('@discordjs/builder');
+client.on('interactionCreate', async interaction => {
+    if (interaction.isCommand()) return;
 
+    if (interaction.commandName === 'spam') {
+        await interaction.deferreply();
+        await wait(100)
+        await interaction.editReply('spam');
+    }
+});
+client.on('interactionCreate', async interaction => {
+    if (interaction.isCommand()) return;
+
+    if (interaction.commandName === 'spam') {
+        await interaction.deferreply();
+        await wait(100)
+        await interaction.editReply('spam');
+    }
+});
+client.on('interactionCreate', async interaction => {
+    if (interaction.isCommand()) return;
+
+    if (interaction.commandName === 'spam') {
+        await interaction.deferreply();
+        await wait(100)
+        await interaction.editReply('spam');
+    }
+});
+client.on('interactionCreate', async interaction => {
+    if (interaction.isCommand()) return;
+
+    if (interaction.commandName === 'spam') {
+        await interaction.deferreply();
+        await wait(100)
+        await interaction.editReply('spam');
+    }
+});
+client.on('interactionCreate', async interaction => {
+    if (interaction.isCommand()) return;
+
+    if (interaction.commandName === 'spam') {
+        await interaction.deferreply();
+        await wait(100)
+        await interaction.editReply('spam');
+    }
+});
+client.on('interactionCreate', async interaction => {
+    if (interaction.isCommand()) return;
+
+    if (interaction.commandName === 'spam') {
+        await interaction.deferreply();
+        await wait(100)
+        await interaction.editReply('spam');
+    }
+});
+client.on('interactionCreate', async interaction => {
+    if (interaction.isCommand()) return;
+
+    if (interaction.commandName === 'spam') {
+        await interaction.deferreply();
+        await wait(100)
+        await interaction.editReply('spam');
+    }
+});
+client.on('interactionCreate', async interaction => {
+    if (interaction.isCommand()) return;
+
+    if (interaction.commandName === 'spam') {
+        await interaction.deferreply();
+        await wait(100)
+        await interaction.editReply('spam');
+    }
+});
+client.on('interactionCreate', async interaction => {
+    if (interaction.isCommand()) return;
+
+    if (interaction.commandName === 'spam') {
+        await interaction.deferreply();
+        await wait(100)
+        await interaction.editReply('spam');
+    }
+});
+client.on('interactionCreate', async interaction => {
+    if (interaction.isCommand()) return;
+
+    if (interaction.commandName === 'spam') {
+        await interaction.deferreply();
+        await wait(100)
+        await interaction.editReply('spam');
+    }
+});
+client.on('interactionCreate', async interaction => {
+    if (interaction.isCommand()) return;
+
+    if (interaction.commandName === 'spam') {
+        await interaction.deferreply();
+        await wait(100)
+        await interaction.editReply('spam');
+    }
+});
+client.on('interactionCreate', async interaction => {
+    if (interaction.isCommand()) return;
+
+    if (interaction.commandName === 'spam') {
+        await interaction.deferreply();
+        await wait(100)
+        await interaction.editReply('spam');
+    }
+});
 client.on('interactionCreate', async interaction => {
     if (interaction.isCommand()) return;
 
